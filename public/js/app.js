65341ccc9dd258cbd03b0a7f895a744317e49ddc
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-22 21:46:46 -0600

    Lecture 59: Wiring up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,74 @@ async function getForecast(address) {
     return json;
 }
 
+// local references to the form and the input
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('#location');
+
+// local reference to all parapgraphs
+const locationResult = document.querySelector('#location-result');
+const forecastResult = document.querySelector('#forecast-result');
+const errorResult = document.querySelector('#error-result');
+const loading = document.querySelector('#loading');
+
+// add submit event listener to the form
+weatherForm.addEventListener('submit', (e) => {
+    
+    // prevent automatic reloading on form submission (natural HTML behavior)
+    e.preventDefault();
+
+    // make appear the loading... paragraph
+    loading.style.display = 'block';
+
+    // get the value from the input
+    const location = search.value;
+
+    // call the async function to wait for the resolved (forecast) data to come in
+    getForecast(location)
+        .then(json => {
+
+            // when a response is provided, disappear the loading... paragraph again
+            loading.style.display = 'none';
+
+            // if error exists
+            if (json.error) {
+
+                // display the error paragraph and set its text
+                errorResult.style.display = 'block';
+                errorResult.textContent = json.error;
+
+                // set a 3s timeout to disappear the alert box
+                setTimeout(() => {
+                    errorResult.style.display = 'none';
+                }, 3000);
+
+                return;
+
+            }
+
+            // if error does not exist, make appear the result boxes and set their text content with the
+            // fetched data
+            locationResult.style.display = 'block';
+            forecastResult.style.display = 'block';
+            locationResult.textContent = json.location;
+            forecastResult.textContent = json.forecast;
+
+        });
+
+});
+
 // get the Forecast Promise and use then() to access the final response and check for error property
-getForecast('Boston')
-    .then(json => {
+// getForecast('Boston')
+//     .then(json => {
 
-        if (json.error) {
-            return console.log(json.error);
-        }
+//         if (json.error) {
+//             return console.log(json.error);
+//         }
 
-        console.log(`Location: ${json.location}`);
-        console.log(`Forecast: ${json.forecast}`);
+//         console.log(`Location: ${json.location}`);
+//         console.log(`Forecast: ${json.forecast}`);
 
-    });
+//     });
 
 // async function: returns a promise
 async function getPuzzle() {
